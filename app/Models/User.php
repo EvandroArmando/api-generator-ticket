5893710d445a9a84772c02b1b2fe515a05dbbecb
EvandroArmando <53578532+EvandroArmando@users.noreply.github.com>
2024-06-19 19:31:51 -0300

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,12 +9,12 @@ use Illuminate\Notifications\Notifiable;
 use MongoDB\Laravel\Eloquent\Model as Eloquent;
 
 
-class User extends Eloquent
+class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    protected $connection = 'mongodb';
-    protected $table = 'users';
+    //protected $connection = 'mongodb';
+    //protected $table = 'users';
 
     /**
      * The attributes that are mass assignable.
